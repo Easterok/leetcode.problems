7981cc11c699780747b0365eb75dfa74f6285a9b
Easterok <beskrovnyk@gmail.com>
2021-07-07 21:58:24 +0300

    solved: three-sum-closest

--- python/three-sum-closest.py
+++ python/three-sum-closest.py
@@ -17,4 +17,30 @@ from typing import List
 
 class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
-        pass
+        sorted_nums = sorted(nums)
+        closest = sorted_nums[0] + sorted_nums[1] + sorted_nums[2]
+
+        def get_distance(candidate):
+            return max(target, candidate) - min(target, candidate)
+
+        def is_closest(candidate):
+            return get_distance(closest) > get_distance(candidate)
+
+        for i in range(len(nums) - 2):
+            l = i + 1
+            r = len(nums) - 1
+
+            while l < r:
+                candidate = sorted_nums[i] + sorted_nums[l] + sorted_nums[r]
+
+                if candidate == target:
+                    return target
+
+                if is_closest(candidate):
+                    closest = candidate
+                elif candidate < target:
+                    l += 1
+                else:
+                    r -= 1
+        
+        return closest
