728f6c2413cc2650a9b52312a99d67142945037d
Easterok <beskrovnyk@gmail.com>
2021-07-07 21:21:57 +0300

    solved: three-sum

--- python/three-sum.py
+++ python/three-sum.py
@@ -23,4 +23,39 @@ from typing import List
 
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        pass
+        result = []
+
+        if len(nums) < 3:
+            return result
+        
+        sorted_nums = sorted(nums)
+
+        for i in range(len(nums) - 2):
+            if i > 0 and sorted_nums[i] == sorted_nums[i - 1]:
+                continue
+
+            l = i + 1
+            r = len(nums) - 1
+
+            while l < r:
+                s = sorted_nums[i] + sorted_nums[l] + sorted_nums[r]
+
+                if s == 0:
+                    result.append([sorted_nums[i], sorted_nums[l], sorted_nums[r]])
+                    l += 1
+                    r -= 1
+
+                    while l < r and sorted_nums[l] == sorted_nums[l - 1]:
+                        l += 1
+                    
+                    while l < r and sorted_nums[r] == sorted_nums[r + 1]:
+                        r -= 1
+
+                elif s < 0:
+                    l += 1
+                else:
+                    r -= 1
+
+        return result
+
+Solution().threeSum([-1,0,1,2,-1,-4])
