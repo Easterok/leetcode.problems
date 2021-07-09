256321162d06f55066866958fe334c2c7d5125ba
Easterok <beskrovnyk@gmail.com>
2021-07-10 02:59:43 +0300

    solved: remove-duplicates

--- python/remove-duplicates.py
+++ python/remove-duplicates.py
@@ -47,4 +47,14 @@ from typing import List
 
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        pass
+        if len(nums) == 0:
+            return 0
+
+        index = 0
+
+        for i in range(1, len(nums)):
+            if nums[index] != nums[i]:
+                index += 1
+                nums[index] = nums[i]
+
+        return index + 1
